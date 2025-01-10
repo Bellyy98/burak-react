51d9c335c52f9e37575014841d75300ae9bb7f0a
Bekzod <bekzodabdupatayev@gmail.com>
2025-01-10 18:44:53 +0900

    feat: create routing sistem

--- src/index.tsx
+++ src/index.tsx
@@ -1,29 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
-import App from './app/App';
-import reportWebVitals from './reportWebVitals';
-import CssBaseline from '@mui/material/CssBaseline';
-import { ThemeProvider } from '@mui/material/styles';
-import theme from './app/material/MaterialTheme';
-import './css/index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
+import App from "./app/App";
+import reportWebVitals from "./reportWebVitals";
+import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "./app/MaterialTheme";
+import {BrowserRouter as Router,} from "react-router-dom";
 
+import "./css/index.css";
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-    <ThemeProvider theme={theme}> 
-     <CssBaseline />
-     <App />
-    </ThemeProvider>
-     
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Router>
+          <App />
+        </Router>
+      </ThemeProvider>
     </Provider>
   </React.StrictMode>,
-   document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+// declarative and component-based
