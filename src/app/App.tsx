4e556ceb832fd8f9417f872005d74d9121ce61a7
Bekzod <bekzodabdupatayev@gmail.com>
2025-01-14 15:44:00 +0900

    fix: organize headee and footer components

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,34 +2,21 @@ import React from "react";
 import "../css/app.css";
 import { Box, Button, Container, Stack, Typography } from "@mui/material";
 import {} from "@mui/icons-material"
-import {Link, Route, Switch, } from "react-router-dom"
+import {Link, Route, Switch, useLocation, } from "react-router-dom"
 import { HomePage } from "./screens/homePage";
 import { ProductsPage } from "./screens/productsPage";
 import { OrdersPage } from "./screens/ordersPage";
 import { UserPage } from "./screens/userPage";
+import { HomeNavbar } from "./components/headers/HomeNavbar";
+import { OtherNavbar } from "./components/headers/OtherNavbar";
+import { Footer } from "./components/footer";
 
 
 function App() {
+  const location = useLocation()
   return (
-    <div>
-    <nav>
-      <ol>
-        <li>
-          <Link to="/">HomePage</Link>
-        </li> 
-        <li>
-          <Link to="/products">ProductsPage</Link>
-          </li>
-        <li>
-          <Link to="/orders">OrdersPage</Link>
-        </li>
-        <li>
-          <Link to="/member-page">UserPage</Link>
-        </li> 
-      </ol>
-    </nav>
-
-
+    <>
+     { location.pathname === "/" ? <HomeNavbar/> : <OtherNavbar/>}
     <Switch>
       <Route path="/products">
         < ProductsPage />
@@ -44,7 +31,8 @@ function App() {
         <HomePage />
       </Route>
     </Switch>
-  </div>
+    <Footer/>
+  </>
   );
 }
 
