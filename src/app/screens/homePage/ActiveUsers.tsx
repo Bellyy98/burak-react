e6e5149a28b4b04fb272c63575a58d3ba8120a27
Bekzod <bekzodabdupatayev@gmail.com>
2025-01-17 16:23:28 +0900

    feat: develop active users sectional component

--- src/app/screens/homePage/ActiveUsers.tsx
+++ src/app/screens/homePage/ActiveUsers.tsx
@@ -1,9 +1,53 @@
 import React from "react";
-import { Container } from "@mui/material";
+import { Box, Container, Stack } from "@mui/material";
+import Card from '@mui/joy/Card';
+import { CardContent, CssVarsProvider, Divider, IconButton, Typography } from "@mui/joy"
+import CardOverflow from '@mui/joy/CardOverflow';
+import AspectRatio from "@mui/joy/AspectRatio";
+
+
+const activeUsers  = [
+    { memberNick: "Martin", memberImage: "/img/martin.webp"},
+    { memberNick: "Justin", memberImage: "/img/justin.webp"},
+    { memberNick: "Rose", memberImage: "/img/rose.webp"},
+    { memberNick: "Nusret", memberImage: "/img/nusret.webp"},
+]
 
 export default function ActiveUsers () {
     return (
-    <div>
-        <Container>AtiveUsers</Container>
+    <div className="active-users-frame">
+        <Container>
+            <Stack className="main">
+             <Box className="category-title" >Active Users</Box>
+             <Stack className="cards-frame">
+             {activeUsers.length !== 0 ? (
+                    activeUsers.map((ele, index) => {
+                        return (
+               <CssVarsProvider>
+               <Card variant="outlined" sx={{ width: 320 }}>
+                <CardOverflow>
+                <AspectRatio  className="membeer-image1">
+                    <img src={ele.memberImage} alt=""/>
+                 </AspectRatio>
+                </CardOverflow>
+                 <CardOverflow variant="soft" sx={{ bgcolor: "white" }}>
+                    <Typography
+                        level="h2"
+                        textAlign={"center"}
+                        fontSize={"lg"}
+                        textColor={"0,0,0"}
+                        className="member-nickname"
+                        >
+                        {ele.memberNick}
+                    </Typography>
+                </CardOverflow>
+    </Card>
+               </CssVarsProvider>
+              )
+            })
+        ) : ( <Box className="no-data"> No Active Users!</Box>)}
+        </Stack>
+            </Stack>
+        </Container>
     </div>)
 }
